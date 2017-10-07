0c701f812071fe1a7f2c9d5ab0a7b36ca333b684
Julian Schurhammer <julian.schurhammer@gmail.com>
2017-10-07 15:09:44 +1300

    physobject extends positionable

--- src/main/java/physics/PhysObject.java
+++ src/main/java/physics/PhysObject.java
@@ -2,46 +2,67 @@ package physics;
 
 import common.AABB;
 import common.Collidable;
+import common.Positionable;
 import common.Vector;
 
-public abstract class PhysObject {
-	private AABB bounds;
-	private Collidable collisionCallback;
-	
-	public PhysObject(AABB bounds) {
-		this.bounds = bounds;
-	}
-	
-	public void setCollisionCallback(Collidable c) {
-		collisionCallback = c;
-	}
+public abstract class PhysObject implements Positionable {
+    private AABB aabb;
+    protected Collidable collisionCallback;
 
-	public abstract AABB getBoundingBox();
+    public PhysObject(AABB aabb) {
+        this.aabb = aabb;
+    }
+    
+    /**
+     * dt is the time passed, in seconds, since advance was last called.
+     * @param dt change in time (seconds)
+     */
+    public abstract void advance(double dt);
+    
+    public void resolveCollision(PhysObject o) {
+        Vector[] collision = aabb.resolveCollision(o.aabb);
+        if(collision != null) {
+            if(collisionCallback != null) {
+                collisionCallback.collide(o.collisionCallback, collision[0]);
+            }
+            if(o.collisionCallback != null) {
+                o.collisionCallback.collide(collisionCallback, collision[1]);
+            }
+        }
+    }
+    
+    @Override
+    public AABB getBoundingBox() {
+        return aabb;
+    }
 
-	public abstract Vector getPosition();
+    @Override
+    public Vector getPosition() {
+        return aabb.center;
+    }
 
-	public abstract void setPosition(Vector p);
-	
-	public void applyForce(Vector f) {
-		bounds.applyForce(f);
-	}
+    @Override
+    public Vector getVelocity() {
+        return aabb.velocity;
+    }
 
-	
-	/**
-	 * dt is the time passed, in seconds, since advance was last called.
-	 * @param dt change in time (seconds)
-	 */
-	public abstract void advance(double dt);
-	
-	public void resolveCollision(PhysObject o) {
-		Vector[] collision = bounds.resolveCollision(o.bounds);
-		if(collision != null) {
-			if(collisionCallback != null) {
-				collisionCallback.collide(o.collisionCallback, collision[0]);
-			}
-			if(o.collisionCallback != null) {
-				o.collisionCallback.collide(collisionCallback, collision[1]);
-			}
-		}
-	}
+    @Override
+    public void setPosition(Vector p) {
+        aabb.center = p;
+    }
+
+    @Override
+    public void setVelocity(Vector p) {
+        aabb.velocity = p;
+    }
+
+    @Override
+    public void applyForce(Vector f) {
+        aabb.applyForce(f);
+    }
+
+    @Override
+    public void setCollisionCallback(Collidable c) {
+        this.collisionCallback = c;
+    }
 }
